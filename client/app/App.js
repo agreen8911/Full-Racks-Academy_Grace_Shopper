c65345aac1bff710ae31e18cb7c4acfdec312be5
DMJennings <dakota96j@gmail.com>
2023-06-14 11:19:48 -0600

    added barebones header and footer to be displayed on all pages

--- client/app/App.js
+++ client/app/App.js
@@ -2,13 +2,20 @@ import React from 'react';
 
 import Navbar from '../features/navbar/Navbar';
 import AppRoutes from './AppRoutes';
+import Footer from '../features/footer/Footer'
 
 const App = () => {
   return (
-    <div>
-      <Navbar />
-      {/* <AppRoutes /> */}
-    </div>
+    <>
+      <div>
+        <Navbar />
+        {/* <AppRoutes /> */}
+        
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </>
   );
 };
 
